49c5f15444284342dfd3a5a5867581508419d8ad
mikezks <23002661+mikezks@users.noreply.github.com>
2024-11-14 17:49:29 +0100

    signal inputs

--- projects/flight/src/app/booking/ui-flight/flight-card/flight-card.component.ts
+++ projects/flight/src/app/booking/ui-flight/flight-card/flight-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, input, model } from '@angular/core';
 import { Flight } from '../../logic-flight';
 import { NgStyle, DatePipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
@@ -10,23 +10,23 @@ import { RouterLink } from '@angular/router';
     template: `
       <div
         class="card"
-        [ngStyle]="{ 'background-color': selected ? 'rgb(204, 197, 185)' : 'white' }"
+        [ngStyle]="{ 'background-color': selected() ? 'rgb(204, 197, 185)' : 'white' }"
       >
         <div class="card-header">
-          <h2 class="card-title">{{ item?.from }} - {{ item?.to }}</h2>
+          <h2 class="card-title">{{ item()?.from }} - {{ item()?.to }}</h2>
         </div>
 
         <div class="card-body">
-          <p>Flight-No.: {{ item?.id }}</p>
-          <p>Date: {{ item?.date | date : "dd.MM.yyyy HH:mm" }}</p>
+          <p>Flight-No.: {{ item()?.id }}</p>
+          <p>Date: {{ item()?.date | date : "dd.MM.yyyy HH:mm" }}</p>
           <p>
             <button
               (click)="toggleSelection()"
               class="btn btn-info btn-sm"
               style="min-width: 85px; margin-right: 5px"
-            >{{ selected ? "Remove" : "Select" }}</button>
+            >{{ selected() ? "Remove" : "Select" }}</button>
             <a
-              [routerLink]="['../edit', item?.id]"
+              [routerLink]="['../edit', item()?.id]"
               class="btn btn-success btn-sm"
               style="min-width: 85px; margin-right: 5px"
             >Edit</a>
@@ -42,17 +42,15 @@ import { RouterLink } from '@angular/router';
     imports: [NgStyle, RouterLink, DatePipe]
 })
 export class FlightCardComponent {
-  @Input() item?: Flight;
-  @Input() selected = false;
-  @Output() selectedChange = new EventEmitter<boolean>();
+  readonly item = input<Flight>();
+  readonly selected = model(false);
   @Output() delayTrigger = new EventEmitter<Flight>();
 
   toggleSelection(): void {
-    this.selected = !this.selected;
-    this.selectedChange.emit(this.selected);
+    this.selected.set(!this.selected());
   }
 
   delay(): void {
-    this.delayTrigger.emit(this.item);
+    this.delayTrigger.emit(this.item());
   }
 }
